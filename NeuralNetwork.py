e013233aa551c7ef42b3320635016527de5ef845
AbogAscended <jlevine@cpp.edu>
2025-03-14 20:55:41 -0700

    #

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -21,13 +21,13 @@ class CRNN(nn.Module):
         
         self.policy = nn.Linear(gru_hidden_size, num_actions)
 
-    def forward(self, x, hidden_state):
+    def forward(self, x, hidden):
         batch_size = x.size(0)
         
-        cnn_features = self.cnn(x.view(batch_size, *x.shape[2:]))
-        cnn_features = cnn_features.unsqueeze(0)
+        cnn_out = self.cnn(x.view(batch_size, *x.shape[2:]))
+        cnn_out = cnn_out.unsqueeze(0)
         
-        gru_out, new_hidden = self.gru(cnn_features, hidden_state)
+        gru_out, new_hidden = self.gru(cnn_out, hidden)
         
         logits = self.policy(gru_out.squeeze(0))
         return logits, new_hidden
